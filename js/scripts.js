2e270379b5a218ee9547fd059f00c889801895ff
Brian Johnson <jbrian93@ymail.com>
2021-05-22 12:53:18 -0400

    Week 4 Assignment

--- js/scripts.js
+++ js/scripts.js
@@ -11,4 +11,23 @@ $(function() {
             $("#carouselButton").children("i").addClass("fa-pause");
         }
     });
+            $("#reserveButton").click(function() {
+                $("#reserveModal").modal("show");
+            });
+
+            $("#loginButton").click(function() {
+                $("#loginModal").modal("show");
+            });
 });
+
+// $(function() {
+//     $("#reserveButton").click(function() {
+//         $("#reserveModal").modal("show");
+//     })
+// })
+
+// $(function() {
+//     $("#loginButton").click(function() {
+//         $("#loginModal").modal("show");
+//     })
+// })
